cd8767154938c4ed6b13a0da807b29991eb80b2b
andr-ew <andrewcshike@gmail.com>
2025-05-21 10:52:45 -0500

    init

---
+++ ndlseggs.lua
@@ -0,0 +1,14 @@
+-- ndlseggs
+--
+-- ndls + eggs
+--
+-- use arc key to switch scripts
+--
+-- version 0.0.1 @andrew
+--
+-- required: grid (any size)
+--           arc 2025
+--
+-- documentation:
+-- github.com/andr-ew/ndls
+-- github.com/andr-ew/eggs
